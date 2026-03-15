1363042a61d8e6692d5a186af4e24f8fbd3de693
qgmzhny <3021452491@qq.com>
2026-03-15 19:39:55 +0800

    match和case构造分支结构

--- To100/004.py
+++ To100/004.py
@@ -32,3 +32,16 @@ else:
         print('肥胖')
 
 # match和case构造分支结构
+status_code = int(input('响应状态码: '))
+match status_code:
+    case 400: description = 'Bad Request'
+    case 401: description = 'Unauthorized'
+    case 403: description = 'Forbidden'
+    case 404: description = 'Not Found'
+    case 405: description = 'Method Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('状态码描述:', description)
+
+# 在 Python 的 match-case 语句中，_（下划线）是一个特殊的通配符模式，表示"匹配任何东西"。
