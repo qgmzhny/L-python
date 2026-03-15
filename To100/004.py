09c898b6e290b4da25fb7622595c0a9dad572fa9
qgmzhny <3021452491@qq.com>
2026-03-15 19:52:38 +0800

    match-case 语句中|表示

--- To100/004.py
+++ To100/004.py
@@ -44,4 +44,18 @@ match status_code:
     case _: description = 'Unknown Status Code'
 print('状态码描述:', description)
 
-# 在 Python 的 match-case 语句中，_（下划线）是一个特殊的通配符模式，表示"匹配任何东西"。
+# 在 Python 的 match-case 语句中，_（下划线）是一个特殊的通配符模式，表示"匹配任何东西"
+
+
+status_code = int(input('响应状态码'))
+match status_code:
+    case 400 | 405: description = 'Invalid Request'
+    case 401 | 403 | 404: description = 'Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('状态码描述',description)
+
+# python在match-case 语句中|表示或
+
+
