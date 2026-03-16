3701a959c9f5a22cc70a93b28bf6e0464553ae47
qgmzhny <3021452491@qq.com>
2026-03-16 23:27:54 +0800

    最大公约数

--- To100/005.py
+++ To100/005.py
@@ -147,3 +147,9 @@ else:
         if num1 % share_nums == 0 and num2 % share_nums == 0:
             print(f'最大公约数: {share_nums}')
 
+x = int(input('x = '))
+y = int(input('y = '))
+while y % x != 0:
+    x, y = y % x, x
+print(f'最大公约数: {x}')
+
