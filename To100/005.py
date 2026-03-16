57c0a8f8b06be69863321a71ee4a04373b537809
qgmzhny <3021452491@qq.com>
2026-03-16 19:50:00 +0800

    循环结构

--- To100/005.py
+++ To100/005.py
@@ -1,10 +1,27 @@
 # day005
+import time
+
 print("day005")
 
 # 循环结构
 # for-in循环
 # 明确知道循环执行的次数
 
+# import time
+for i in range(40):
+    print('hello world!')
+    time.sleep(1)
+
+# range 对象并不存储每一个数字
+# 它存储的是 起始值、结束值和步长
+# range(40)：可以用来产生0到39范围的整数，取不到40
+# sleep 单位是s
+
+# Python 的编程惯例，我们通常把循环变量命名为 _
+# import time
+for _ in range(40):
+    print('hello world!')
+    time.sleep(0.1)
 
 
 # while循环
