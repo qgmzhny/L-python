13f1a14d82978d55ac5c1e7afbbbc5dfb7656a55
qgmzhny <3021452491@qq.com>
2026-03-17 19:27:00 +0800

    寻找水仙花数

--- To100/006.py
+++ To100/006.py
@@ -34,3 +34,25 @@ for i in range(1, 19):
 #     a, b = b, a + b
 #     print(a)
 
+# 寻找水仙花数
+# 找出 100 到 999 范围内的所有水仙花数
+# 水仙花数:
+# 一个 N位非负整数，
+# 其各位数字的N次方和刚好等于该数本身
+for _ in range(100, 1000):
+    b3 = int(_ / 100)
+    b2 = int((_ - 100 * b3) / 10)
+    b1 = _ - 100 * b3 - 10 * b2
+    if b1 ** 3 + b2 ** 3 + b3 ** 3 == _:
+        print(f'{_}是水仙花数')
+
+# for num in range(100, 1000):
+#     low = num % 10
+#     mid = num // 10 % 10
+#     high = num // 100
+#     if num == low ** 3 + mid ** 3 + high ** 3:
+#         print(num)
+
+# // 为整除符号
+
+
