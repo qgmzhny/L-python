4956aaa0fa556ece174083134de675306df37c5a
qgmzhny <3021452491@qq.com>
2026-03-17 19:04:17 +0800

    斐波那契数列

--- To100/006.py
+++ To100/006.py
@@ -14,6 +14,23 @@ for _ in range(2, 100):
     else:
         print(f'{_}不是素数')
 
+# 斐波那契数列
+# 输出斐波那契数列中的前 20 个数
+# 斐波那契数列的特点是数列的前两个数都是 1，从第三个数开始，每个数都是它前面两个数的和
 
+f1 = 1
+f2 = 1
+print(f'f1 = {f1}')
+print(f'f2 = {f2}')
+for i in range(1, 19):
+    fn = f1 + f2
+    temp = fn
+    f1 = f2
+    f2 = temp
+    print(f'f{i + 2} = {fn}')
 
+# a, b = 0, 1
+# for _ in range(20):
+#     a, b = b, a + b
+#     print(a)
 
