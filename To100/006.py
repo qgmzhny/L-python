d4e6d2ed48b599b623286e1a68b8c0ce7f62bf80
qgmzhny <3021452491@qq.com>
2026-03-17 18:47:34 +0800

    100以内的素数

--- To100/006.py
+++ To100/006.py
@@ -1,3 +1,19 @@
 # day006
-
 print("day006")
+
+# 100以内的素数
+for _ in range(2, 100):
+    prime_status = True
+    half_factor = int(_ ** 0.5)
+    for i in range(2, half_factor + 1):
+        if _ % i == 0:
+            prime_status = False
+            break
+    if prime_status:
+        print(f'{_}是素数')
+    else:
+        print(f'{_}不是素数')
+
+
+
+
