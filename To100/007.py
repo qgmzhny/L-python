1ff2da5404b8edb57dd21fd49dadc7805ab33db7
qgmzhny <3021452491@qq.com>
2026-03-18 19:14:03 +0800

    list列表

--- To100/007.py
+++ To100/007.py
@@ -1,4 +1,29 @@
 # day007
 print("day007")
 
+# 常用数据结构
+
+# 列表
+# 用一个变量来保存多个数据
+# 统一的代码对多个数据进行操作
+
+# 列表（list）
+# 在 Python 中
+# 使用[]字面量语法来定义列表
+# 列表中的多个元素用逗号进行分隔
+
+items1 = [9, 5, 2, 7]
+items2 = ['list', 'for-in', 'while']
+items3 = [101, 'jack', False]
+print(items1)
+print(items2)
+print(items3)
+print(type(items1))
+print(type(items2))
+print(type(items3))
+
+
+
+
+
 
