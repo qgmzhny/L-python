cea87b6f8d99cecb9fa3a5232a3958bbe01c27e0
qgmzhny <3021452491@qq.com>
2026-03-19 21:42:21 +0800

    双色球随机选号-rich表

--- To100/007.py
+++ To100/007.py
@@ -316,10 +316,34 @@ for ball in red:
 for ball in blue:
     print(f'\033[034m{ball:0>2d}\033[0m')
 
+# import random
 
+from rich.console import Console
+from rich.table import Table
 
+# 创建控制台
+console = Console()
 
+n = int(input('生成几注号码: '))
+red_balls = [i for i in range(1, 34)]
+blue_balls = [i for i in range(1, 17)]
 
+# 创建表格并添加表头
+table = Table(show_header=True)
+for col_name in ('序号', '红球', '蓝球'):
+    table.add_column(col_name, justify='center')
 
+for i in range(n):
+    selected_balls = random.sample(red_balls, 6)
+    selected_balls.sort()
+    blue_ball = random.choice(blue_balls)
+    # 向表格中添加行（序号，红色球，蓝色球）
+    table.add_row(
+        str(i + 1),
+        f'[red]{" ".join([f"{ball:0>2d}" for ball in selected_balls])}[/red]',
+        f'[blue]{blue_ball:0>2d}[/blue]'
+    )
 
+# 通过控制台输出表格
+console.print(table)
 
