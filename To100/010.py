fd688ec075508dd59a4947385982b93bf5c3c589
qgmzhny <3021452491@qq.com>
2026-03-25 20:56:06 +0800

    集合运算-二元运算

--- To100/010.py
+++ To100/010.py
@@ -47,4 +47,25 @@ ikun = {'唱', '跳', 'rap', '篮球'}
 print('唱' in ikun)
 print('music' not in ikun)
 
+# 二元运算
+# 交集、并集、差集、对称差
+set1 = {1, 2, 3, 4, 5, 6, 7}
+set2 = {2, 4, 6, 8, 10}
 
+# 交集
+print(set1 & set2)
+print(set1.intersection(set2))
+
+# 并集
+print(set1 | set2)
+print(set1.union(set2))
+
+# 差集
+print(set1 - set2)
+print(set2 - set1)
+print(set1.difference(set2))
+print(set2.difference(set1))
+
+# 对称差
+print(set1 ^ set2)
+print(set1.symmetric_difference(set2))
