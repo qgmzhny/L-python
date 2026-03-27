838f5cba738f51550ac4eced1537a20ca9f097c9
qgmzhny <3021452491@qq.com>
2026-03-27 16:53:30 +0800

    不可变集合

--- To100/010.py
+++ To100/010.py
@@ -132,4 +132,14 @@ print(set1.isdisjoint(set3))
 
 # 不可变集合
 # frozenset
+fset1 = frozenset({2, 3, 5, 7})
+fset2 = frozenset(range(5))
+print(fset1)
+print(fset2)
+print(fset1 & fset2)
+print(fset1 | fset2)
+print(fset1 - fset2)
+print(fset1 < fset2)
+
+
 
