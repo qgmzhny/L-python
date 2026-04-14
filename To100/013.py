9889d2a69f6a30e89ae2cfb77e436027b599180d
qgmzhny <3021452491@qq.com>
2026-04-14 17:50:29 +0800

    判断素数

--- To100/013.py
+++ To100/013.py
@@ -21,7 +21,7 @@ for _ in range(5):
     print(VerCode())
 
 # import random
-def generate_code(*, args):
+def generate_code(*, args ):
     nums = list(range(10))
     symbol = [chr(i) for i in list(range(97, 123)) + list(range(65, 91))]
     return (''.join(str(_) for _ in random.choices(nums + symbol, k= args)))
@@ -31,3 +31,33 @@ print('参数个数：')
 print(generate_code(args = int(input())))
 
 
+# 判断素数
+# 设计一个判断给定的大于1的正整数是不是质数的函数。
+# 质数是只能被1和自身整除的正整数（大于1），
+# 如果一个大于 1 的正整数N是质数，
+# 那就意味着在 2 到 N−1之间都没有它的因子
+
+def is_prime(num):
+    if num <= 1:
+        return 0
+    elif num == 4:
+        return 1
+    else:
+        for _ in range(2, num // 2):
+            if num % _ == 0:
+                return 1
+    return 2
+
+def ST_Judge(num):
+    if num == 0:
+        print('数据需要大于1的整数')
+    elif num == 1:
+        print('是合数')
+    else:
+        print('是质数')
+
+
+print('请输入需要判断的数据')
+num = int(input())
+temp = is_prime(num)
+ST_Judge(temp)
