a6e44dbef576982af5a62d99befdcee866e80ecc
qgmzhny <3021452491@qq.com>
2026-04-14 20:12:07 +0800

    双色球随机选号

--- To100/013.py
+++ To100/013.py
@@ -1,4 +1,3 @@
-import random
 import string
 
 # day013函数应用
@@ -97,3 +96,48 @@ num2 = int(input())
 print(
     f'{num1}和{num2}的最大公约数为：{gcd(num1, num2)};\t最小公倍数为：{lcm(num1, num2)}'
 )
+
+
+# 数据统计
+
+
+# 双色球随机选号
+# 将生成随机号码和输出一组号码的功能分别封装到两个函数中，
+# 通过调用函数实现机选N注号码的功能
+
+# 双色球是由中国福利彩票发行管理中心发售的乐透型彩票，
+# 每注投注号码由6个红色球和1个蓝色球组成。
+# 红色球号码从1到33中选择，
+# 蓝色球号码从1到16中选择。
+# 每注需要选择6个红色球号码和1个蓝色球号码
+
+import random
+
+
+def generate_balls() -> list:
+    red_ball = [i for i in range(1, 34)]
+    blue_ball = [i for i in range(1, 17)]
+    win_ball = []
+    for _ in range(6):
+        win_ball.append(random.choice(red_ball))
+    win_ball.append(random.choice(blue_ball))
+    return win_ball
+
+
+def show_numbers(nums: int):
+    for _ in range(nums):
+        win_balls = generate_balls()
+        last = win_balls.pop()
+        # 输出选中的红色球
+        for ball in win_balls:
+            print(f'\033[031m{ball:0>2d}\033[0m', end=' ')
+        # 输出选中的蓝色球
+        print(f'\033[034m{last:0>2d}\033[0m')
+
+
+# 买奖
+print('生成几注号码: ')
+nums = int(input())
+# 开奖
+show_numbers(nums)
+
